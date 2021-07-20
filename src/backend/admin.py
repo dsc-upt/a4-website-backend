78adb35f0eee7d540258d41baf11395f547d6382
GramescuDan <gramescudan@yahoo.com>
2021-07-20 15:35:28 +0300

    feat: added contactsadmin to admin.py

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.contrib.admin import register
 
 from backend.models.example import Example
+from backend.models.contactsitem import ContactsItem
 
 
 @register(Example)
+@register(ContactsItem)
 class ExampleAdmin(admin.ModelAdmin):
     pass
+
+class ContactsItemAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'surname', 'email', 'phonenumber')
+
+admin.site.register(ContactsItem, ContactsItemAdmin)
