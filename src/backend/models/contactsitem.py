3977e741d48c8ac85f4b3c37f9a606383a6a5271
GramescuDan <gramescudan@yahoo.com>
2021-07-20 14:30:41 +0300

    feat : Made ContactsItem.py
    it has name, surname, phone number and email

---
+++ src/backend/models/contactsitem.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class ContactsItem(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phonenumber = models.IntegerField(max_length=10)
