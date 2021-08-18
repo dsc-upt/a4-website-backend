e83a46aa6891b0e38faa99835bf311bd45feeff1
Mihai Parvulescu <mihai1309f@gmail.com>
2021-08-18 21:21:12 +0300

    feat: added model, basic admin and migration for FAQ item

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -4,11 +4,20 @@ from django.contrib.admin import register
 from backend.models.example import Example
 from backend.models.settings import Settings
 
+from backend.models.faqitem import FAQItem
+
 
 @register(Example)
 class ExampleAdmin(admin.ModelAdmin):
     pass
 
+
 @register(Settings)
 class ExampleSettings(admin.ModelAdmin):
     pass
+
+
+@register(FAQItem)
+class FAQItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer', 'publish_date')
+    ordering = ['publish_date']
