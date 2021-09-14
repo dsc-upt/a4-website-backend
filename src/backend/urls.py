cd6aa876073c4321ab0b329317a5873e84e3f6cc
Mihai Parvulescu <mihai1309f@gmail.com>
2021-09-14 18:50:42 +0300

    feat: revamped everything

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -2,10 +2,10 @@ from rest_framework.routers import DefaultRouter
 
 from backend.views.example import ExampleViewSet
 
-from backend.views.faqitem import FAQItemViewSet
+from backend.views.faq import FaqViewSet
 
 router = DefaultRouter()
 router.register("examples", ExampleViewSet)
-router.register("faqs", FAQItemViewSet)
+router.register("faqs", FaqViewSet)
 
 backend_urls = router.urls
--- src/backend/views/faqitem.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from backend.models.faqitem import FAQItem
-from backend.serializers.faqitem import FAQItemSerializer
-
-
-class FAQItemViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = FAQItem.objects.all()
-    serializer_class = FAQItemSerializer
