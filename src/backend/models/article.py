2912289cf950528869d38474a6a14caa356edefe
Mario <mateasmario@aol.com>
2021-11-30 20:31:36 +0200

    test

--- src/backend/models/article.py
+++ src/backend/models/article.py
@@ -3,6 +3,6 @@ from django.db import models
 #add slug and image
 class Article(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=250, primary_key=True, blank=True)
+    slug = models.SlugField(max_length=250, primary_key=True, blank=True, null=True)
     content = models.TextField()
     published = models.BooleanField()
