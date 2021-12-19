583b8660f8b328d90f265257cf647c56c2100f1d
Lore Gostian <73692970+loredanagostian16@users.noreply.github.com>
2021-12-19 11:16:10 +0200

    Deleted Tag Model and its corresponding views (#7)

--- src/backend/serializers/tag.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework import serializers
-from backend.models.tag import Tag
-
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = '__all__'
